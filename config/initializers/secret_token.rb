ceb3c2fae70f1d833cdbe67c9778115e0ebe25b6
Riky <riky@Rikys-Mac.local>
2011-10-22 15:45:54 +0200

    primo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestGit::Application.config.secret_token = 'b6272ae36153202e7f9ab71fe6819b6f3db46ec65af12dc5e9b9ec6bfdc9d080a1f94d2279c5a996510c9978b67dbd6537056c18c3706a3470ae26333eddb011'
